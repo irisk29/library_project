1ce4f30eb207609bc98953eef29e4ffb50268369
Iris Kronfeld <iris.kronfeld@gmail.com>
2023-02-22 12:41:21 +0200

    apply styling to edit or add user form and add input checking

--- src/components/EditOrAddUserForm.jsx
+++ src/components/EditOrAddUserForm.jsx
@@ -1,4 +1,7 @@
+import { Button, colors } from "@mui/material";
 import React, { useState } from "react";
+import { Form, FormControl, FormGroup } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import SimpleHeader from "./SimpleHeader";
 
 function AddOrEditUser(props)
@@ -8,9 +11,9 @@ function AddOrEditUser(props)
     function handleSubmit(event)
     {
         //TODO: should save the user in the db (incase of edit it should update and not create)
-        event.preventDefault();
+        //event.preventDefault(); --> prevents navigation
         alert("saved user in db");
-        setNewUser({"fullName": "", "personalID": ""});
+        //setNewUser({"fullName": "", "personalID": ""});
     }
 
     function handleChange(event)
@@ -19,29 +22,33 @@ function AddOrEditUser(props)
         setNewUser((prev) => ({...prev, [name]: value}));
     }
 
+    function checkInputs()
+    {
+        if(newUser.fullName.length === 0 || newUser.personalID.length === 0) return false;
+        return true;
+    }
     return (
         <div>
             {props.name === "" ? <SimpleHeader title="Create New User" /> : null} {/*the cond means this is add mode*/}
-            <div>
-                <form inline onSubmit={handleSubmit}>
-                    <label>Enter your full name:
-                        <input 
-                        type="text" 
-                        name="fullName" 
-                        value={newUser.fullName} 
-                        onChange={handleChange}
-                        />
-                    </label>
-                    <label>Enter your personal ID:
-                        <input 
-                            type="text" 
-                            name="personalID" 
-                            value={newUser.personalID} 
-                            onChange={handleChange}
-                        />
-                    </label>
-                    <input type="submit" />
-                </form>
+            <div className="div-form">
+                <Form>
+                    <FormGroup controlId="formUserName">
+                        <Form.Label style={{ "marginRight": "1rem" }}>Full Name</Form.Label> 
+                        <FormControl type="text" name="fullName" value={newUser.fullName} onChange={handleChange}/>
+                    </FormGroup>
+                    <FormGroup controlId="formPersonalID" style={{"marginTop": "1rem", "marginBottom": "2rem"}}>
+                        <Form.Label style={{ "marginRight": "0.3rem" }}>Personal ID</Form.Label> 
+                        <FormControl type="text" name="personalID" value={newUser.personalID} onChange={handleChange}/>
+                    </FormGroup>
+                    <FormGroup>
+                    {checkInputs() ? 
+                            <Link to="/users">
+                                <Button type="submit" onClick={handleSubmit} variant="contained" size="small" style={{"backgroundColor": colors.amber[500]}}>Submit</Button>
+                            </Link>
+                         : <h6>Please enter full name and/or ID</h6>}
+                         
+                    </FormGroup>
+                </Form>
             </div>
         </div>
     );
