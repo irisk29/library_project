56b4de712ee953dc78d81051519a20b21793a44e
Iris Kronfeld <iris.kronfeld@gmail.com>
2023-02-23 22:16:03 +0200

    update server to allow access

--- LibraryServer/main.py
+++ LibraryServer/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 import os
 import boto3
@@ -9,6 +10,18 @@ load_dotenv()
 # Create a new API Gateway client
 client = boto3.client('apigateway')
 app = FastAPI()
+
+# change the cores to allow access
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 api_gateway_endpoint = os.environ["API_ENDPOINT"]
 headers = {"Content-type": "application/json"}
